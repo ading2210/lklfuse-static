3877fd3f86a29b3702cad1375384a3e7b7e6781e
ading2210 <ading2019@gmail.com>
2024-05-11 21:15:07 -0400

    add fusermount compile script

---
+++ build_fuse.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ ! -f "libfuse-master.zip" ]; then
+  wget "https://github.com/libfuse/libfuse/archive/refs/heads/master.zip" -O libfuse-master.zip
+fi
+
+if [ ! -d "libfuse" ]; then
+  unzip -q libfuse-master.zip
+  mv libfuse-master libfuse
+fi
+
+cd libfuse
+mkdir -p build
+cd build
+meson setup ..
+meson configure --default-library static \
+  -D buildtype=release \
+  -D prefer_static=true \
+  -D c_link_args="-static" \
+  -D cpp_link_args="-static" 
+ninja
+
+mv util/fusermount3 ../../
